856147f3f56a8b2382d7886287b707d7123a628c
Eskinder Romha Fitsim <romha223@gmail.com>
2024-07-09 12:22:30 -0400

    finshed nav now startin body

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Head from "next/head";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
 export default function Home() {
   return (
@@ -10,9 +11,27 @@ export default function Home() {
         <link rel="icon" href="/next.svg" />
       </Head>
 
-      <main>
-        <section className=" bg-blue-700 min-h-screen">
-          <h1>Hello</h1>
+      <main className="bg-white px-10">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl font-league-spartan">developedbyeskinder</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div>
+            <h2>Eskinder Fitsum</h2>
+          </div>
         </section>
       </main>
     </div>
